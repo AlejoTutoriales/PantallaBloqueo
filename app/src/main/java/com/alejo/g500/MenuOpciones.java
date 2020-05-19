bff68ad2bbabcdaa352cc617b8177c39bf514ddd
Alejo <alejo.tec@hotmail.com>
2020-05-18 21:40:20 -0500

    Se actualizó el ícono y se agregó la activty de registro

--- app/src/main/java/com/alejo/g500/MenuOpciones.java
+++ app/src/main/java/com/alejo/g500/MenuOpciones.java
@@ -9,13 +9,14 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MenuOpciones extends AppCompatActivity {
-    private CardView btnPatronBloqueo;
+    private CardView btnPatronBloqueo, btnRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
         btnPatronBloqueo = findViewById(R.id.btnPatronBloqueo);
+        btnRegistro = findViewById(R.id.btnRegistro);
 
         btnPatronBloqueo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +24,14 @@ public class MenuOpciones extends AppCompatActivity {
                 Intent intent = new Intent(MenuOpciones.this, MainActivity.class);
                 intent.putExtra("nuevo_patron", "1");
                 startActivity(intent);
+            }
+        });
+
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuOpciones.this, Registro.class));
             }
         });
 
